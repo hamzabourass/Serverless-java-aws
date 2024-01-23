c59395fdc009d9e2226dfb33af3744d125b860bd
hamzabourass <roxmax224@gmail.com>
2024-01-24 00:41:58 +0100

    Simple lambda function in java

---
+++ src/main/java/ma/DS/SimpleLambda.java
@@ -0,0 +1,11 @@
+package ma.DS;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class SimpleLambda implements RequestHandler<String,String> {
+    @Override
+    public String handleRequest(String input, Context context) {
+        return input.toUpperCase();
+    }
+}
