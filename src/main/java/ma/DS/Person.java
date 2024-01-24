8cdf32d592dc7b1e9f2c24ef9ce551dd9072cda3
hamzabourass <roxmax224@gmail.com>
2024-01-24 14:26:04 +0100

    Simple http lambda function

--- src/main/java/ma/DS/Person.java
+++ src/main/java/ma/DS/Person.java
@@ -1,2 +1,23 @@
-package ma.DS;public class Person {
+package ma.DS;
+
+public class Person {
+
+    private String name;
+    private  String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/ma/DS/SimpleLambda.java
+++ src/main/java/ma/DS/SimpleLambda.java
@@ -6,6 +6,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 public class SimpleLambda implements RequestHandler<String,String> {
     @Override
     public String handleRequest(String input, Context context) {
-        return input.toUpperCase();
+
+        String message = System.getenv("MESSAGE");
+        return message+ " " + input.toUpperCase();
     }
 }
